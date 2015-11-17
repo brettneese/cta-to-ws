5f0ee3a89401ea6360fd7baf4b542056437e570f
Brett Neese <brett@neese.rocks>
2015-11-16 18:38:18 -0600

    clean up app.js

--- app.js
+++ app.js
@@ -28,9 +28,7 @@ app.get('/streamserver', function (req, res) {
 });
 
 
-
 app.get('/static', function (req, res) {
-
   firebaseRef.child("data").once("value", function(snapshot) {
     data = snapshot.val();
     var trains = [];
@@ -49,17 +47,7 @@ app.get('/static', function (req, res) {
 });
 
 
-wss.on('connection', function connection(ws) {
-  var location = url.parse(ws.upgradeReq.url, true);
-  ws.send('connected');
-
-});
-
-
-
-
-firebaseRef.child("data").on("child_changed", function(snapshot) {
-  data = snapshot.val();
+function parseAndSendData(data){
   var trains = [];
   var trainsArray = [];
 
@@ -69,21 +57,30 @@ firebaseRef.child("data").on("child_changed", function(snapshot) {
 
     geojson = GeoJSON.parse(point, {Point: ['lat', 'lon']});
     arcgis = ArcGIS.convert(geojson);
-    console.log(arcgis);
-
       wss.clients.forEach(function each(client) {
         client.send(JSON.stringify(arcgis));
       });
-
   });
 
+}
 
-
-
+// theoretically we'd just to update things from here down
+// send data when changed
+firebaseRef.child("data").on("child_changed", function(snapshot) {
+  parseAndSendData(snapshot.val()); 
 });
 
 
+//send data to start
+wss.on('connection', function connection(ws) {
 
+  firebaseRef.child("data").once("value", function(snapshot) {
+      _.each(snapshot.val(), function(item){
+        parseAndSendData(item); 
+     });
+  });
+
+});
 
 ///server.on('request', app);
 server.listen(port, function () { console.log('Listening on ' + server.address().port)});
